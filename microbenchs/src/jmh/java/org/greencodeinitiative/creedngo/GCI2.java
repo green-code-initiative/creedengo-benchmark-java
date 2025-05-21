b0a6c0c4c1efcb9e063d3b4d8595ba9ead73f85f
Guillaume Raffin <theelectronwill@gmail.com>
2025-05-21 10:22:30 +0200

    add benchmark for GCI2

---
+++ microbenchs/src/jmh/java/org/greencodeinitiative/creedngo/GCI2.java
@@ -0,0 +1,50 @@
+package org.greencodeinitiative.creedngo;
+
+import java.util.Random;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+@State(Scope.Thread)
+public class GCI2 {
+
+    private static long SEED = 123;
+    public int arg;
+
+    @Setup(Level.Iteration)
+    public void setup() {
+        arg = new Random(SEED).nextInt(0, 4);
+        // 0, 1, 2 or 3
+    }
+
+    @Benchmark
+    public int compliant(Blackhole bh) {
+        int nb = arg;
+        switch (nb) {
+            case 0 -> bh.consume(nb);
+            case 1 -> bh.consume(nb);
+            case 2 -> bh.consume(nb);
+            default -> bh.consume(nb);
+        }
+        return nb;
+    }
+
+    @Benchmark
+    public int nonCompliant(Blackhole bh) {
+        int nb = arg;
+        if (nb == 0) {
+            bh.consume(nb);
+        } else if (nb == 1) {
+            bh.consume(nb);
+        } else if (nb == 2) {
+            bh.consume(nb);
+        } else {
+            bh.consume(nb);
+        }
+        return nb;
+    }
+}
